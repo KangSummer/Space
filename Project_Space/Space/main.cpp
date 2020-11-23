e5659e3440d28a3134c02a555e55de95eacc512b
KangMinJae <qmfforsmreo@gmail.com>
2020-11-23 23:20:19 +0900

    - 작업 내용 - 1. 기본 윈도우 생성 2. 소스트리, 깃허브 연동

---
+++ Project_Space/Space/main.cpp
@@ -0,0 +1,164 @@
+#pragma once
+#define WIN32_LEAM_AND_MEAN		// 거의 사용되지 않는 내용을 Windows 헤더에서 제외
+
+#include <Windows.h>
+#include <iostream>
+
+#include "Resource.h"
+
+#define MAX_LOADSTRING 100
+
+WCHAR										szTitle[MAX_LOADSTRING];			// 제목 이름
+WCHAR										szWindowClass[MAX_LOADSTRING];	// 기본 창 클래스 이름
+
+ATOM											MyRegisterClass(HINSTANCE hInstance);
+BOOL											InitInstance(HINSTANCE, int);
+LRESULT CALLBACK			WndProc(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK			About(HWND, UINT, WPARAM, LPARAM);
+
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine, _In_ int nCmdShow)
+{
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+	UNREFERENCED_PARAMETER(hPrevInstance);
+	UNREFERENCED_PARAMETER(lpCmdLine);
+
+	// 전역 문자열을 초기화합니다.
+	LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+	LoadStringW(hInstance, IDC_MY01CLIENT, szWindowClass, MAX_LOADSTRING);
+	MyRegisterClass(hInstance);
+
+	if (!InitInstance(hInstance, nCmdShow))
+	{
+		return FALSE;
+	}
+
+	HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_MY01CLIENT));
+
+	MSG msg;
+
+	while (true)
+	{
+		if (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE))
+		{
+			if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+			{
+				TranslateMessage(&msg);
+				DispatchMessage(&msg);
+			}
+
+			if (msg.message == WM_QUIT)
+			{
+				break;
+			}
+		}
+		else
+		{
+
+		}
+	}
+
+	return (int)msg.wParam;
+}
+
+ATOM MyRegisterClass(HINSTANCE hInstance)
+{
+	WNDCLASSEXW wcex;
+
+	wcex.cbSize						= sizeof(WNDCLASSEXW);
+	wcex.style							= CS_HREDRAW | CS_VREDRAW;
+	wcex.cbClsExtra				= 0;
+	wcex.cbWndExtra				= 0;
+	wcex.hInstance					= hInstance;
+	wcex.hIcon							= LoadIcon(hInstance, MAKEINTRESOURCE(IDI_MY01CLIENT));
+	wcex.hCursor						= LoadCursor(nullptr, IDC_ARROW);
+	wcex.hbrBackground		= (HBRUSH)(COLOR_WINDOW + 1);
+	wcex.lpszMenuName		= nullptr;
+	wcex.lpszClassName		= szWindowClass;
+	wcex.hIconSm					= LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+
+	return RegisterClassExW(&wcex);
+}
+
+// Create global handle
+HINSTANCE		hInst;
+HWND					g_hWnd;
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
+{
+	hInst = hInstance;
+	g_hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
+
+	if (!g_hWnd)		return FALSE;
+
+	ShowWindow(g_hWnd, nCmdShow);
+	UpdateWindow(g_hWnd);
+
+	return TRUE;
+}
+
+
+//  함수: WndProc(HWND, UINT, WPARAM, LPARAM)
+//
+//  용도: 주 창의 메시지를 처리합니다.
+//
+//  WM_COMMAND  - 응용 프로그램 메뉴를 처리합니다.
+//  WM_PAINT    - 주 창을 그립니다.
+//  WM_DESTROY  - 종료 메시지를 게시하고 반환합니다.
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	switch (message)
+	{
+	case WM_COMMAND:
+	{
+		int wmId = LOWORD(wParam);
+		// 메뉴 선택을 구문 분석합니다:
+		switch (wmId)
+		{
+		case IDM_ABOUT:
+			DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
+			break;
+		case IDM_EXIT:
+			DestroyWindow(hWnd);
+			break;
+		default:
+			return DefWindowProc(hWnd, message, wParam, lParam);
+		}
+	}
+	break;
+	case WM_PAINT:
+	{
+		PAINTSTRUCT ps;
+		HDC hdc = BeginPaint(hWnd, &ps);
+		// TODO: 여기에 hdc를 사용하는 그리기 코드를 추가합니다...
+		EndPaint(hWnd, &ps);
+	}
+	break;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	default:
+		return DefWindowProc(hWnd, message, wParam, lParam);
+	}
+	return 0;
+}
+
+// 정보 대화 상자의 메시지 처리기입니다.
+INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	UNREFERENCED_PARAMETER(lParam);
+	switch (message)
+	{
+	case WM_INITDIALOG:
+		return (INT_PTR)TRUE;
+
+	case WM_COMMAND:
+		if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+		{
+			EndDialog(hDlg, LOWORD(wParam));
+			return (INT_PTR)TRUE;
+		}
+		break;
+	}
+	return (INT_PTR)FALSE;
+}
